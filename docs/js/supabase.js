b4a921863d84abae4eeba5f0764e4682909bcef6
RJDdd <cdmx2605@amerike.edu.mx>
2025-05-27 15:22:14 -0600

    pñ

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://lrghqupggsruqgxtgsmo.supabase.co';
-const supabaseKey = 'tu-clave-anon-public'; // Reemplaza con tu clave real
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyZ2hxdXBnZ3NydXFneHRnc21vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjIyNjEsImV4cCI6MjA2MTUzODI2MX0.iLHOy11kz32yWYN6mWRcCnzUfDnu39Zq698iPDBAbTU'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
